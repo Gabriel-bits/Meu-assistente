12794a78f2ce1444e4659c89a5441230d89c4a93
Narutinn <69373580+Gabriel-bits@users.noreply.github.com>
2021-03-25 18:48:03 -0300

    up

--- main.py
+++ main.py
@@ -1,34 +1,56 @@
 import speech_recognition as sr 
 import pyttsx3
+from gtts import gTTS
+import playsound
+import pyaudio
+from vosk import Model, KaldiRecognizer
+import os
+
+model = Model('model')
+rec = KaldiRecognizer(model, 16000)
+
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream.start_stream()
+
+while True:
+    data = stream.read(4000)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        print(rec.Result())
+    else:
+        print(rec.PartialResult())
+
+print(rec.FinalResult())
+
+# def speak(text):
+#     souce = pyttsx3.init()
+#     pyttsx3.voice.Voice(id=None, languages="pt")
+#     souce.say(text)
+#     souce.runAndWait()
 
 
-def speak(text):
-    souce = pyttsx3.init()
-    pyttsx3.voice.Voice(id=None, languages="pt")
-    souce.say(text)
-    souce.runAndWait()
 
+# r = sr.Recognizer()
 
+# with sr.Microphone() as m:
+#     r.adjust_for_ambient_noise(m)
 
-r = sr.Recognizer()
+#     while True:
+#         try:
 
-with sr.Microphone() as s:
-    r.adjust_for_ambient_noise(s)
+#             audioo = r.listen(m)
+#             speech = r.recognize_google(audioo, language='pt')
 
-    while True:
-        try:
-            audioo = r.listen(s)
+#             print(speech)
+#             # speak(speech)
 
-            speech = r.recognize_google(audioo, language='pt')
-
-            print(speech)
-            # speak(speech)
-
-            if speech in "Olá":
-                speak("Oi, tudo bem")
+#             if speech in "Olá":
+#                 speak("Oi, tudo bem")
             
-            if "tudo e voce ?" in speech:
-                speak("aqui ta otimo !")
+#             if "Tudo e você" in speech:
+#                 speak("aqui ta ótimo!")
 
-        except Exception as e:
-            print(e)
+#         except Exception as e:
+#             print(e)
